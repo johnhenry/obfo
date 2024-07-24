cd388139dd731f77c96b171a6f30ac7721e3bf6b
John <john@iamjohnhenry.com>
2024-07-24 11:49:09 -0700

    add flexable type

--- types/obfo.d.ts
+++ types/obfo.d.ts
@@ -1,5 +1,5 @@
 // Assuming obfo is a function that takes a form reference and returns a Promise of an object
 declare module "obfo" {
   // Adjust the parameter and return types based on the actual implementation
-  export default function (formRef: HTMLFormElement): object;
+  export default function <T = object>(formRef: HTMLFormElement): T;
 }
